81ae3176842326ca82154aca579084c5faef0a9a
rams41168 <rams41168@gmail.com>
2026-02-17 10:57:52 +0530

    Add files via upload

---
+++ char.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+char p ='a';
+char q ='b';
+int sum=p+q;
+    printf("%d\n",sum);
+   printf("ASCII value of %c is %d\n",q,q);
+ printf("ASCII value of %c is %d\n",p,p);
+
+    return 0;
+}
