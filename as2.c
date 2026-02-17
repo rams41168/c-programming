555b51f3849d4bdcfbb3058c600bce965678f16e
rams41168 <rams41168@gmail.com>
2026-02-17 10:43:35 +0530

    Add files via upload

---
+++ as2.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main(){
+int a,b,c;
+printf("enter the number");
+scanf("%d %d %d",&a,&b,&c);
+if(a>=b&&a>=c)
+printf("enter the largest number:a");
+{
+if(b>=a&&b>=c)
+printf("enter the largest number:b");
+else
+printf("enter the largest number:c");
+}
+return 0;
+}
+
