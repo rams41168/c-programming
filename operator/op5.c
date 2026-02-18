d7877669e208b5c47fc4bfee4a2ac603508ac2e5
rams41168 <rams41168@gmail.com>
2026-02-18 17:31:09 +0530

    Add files via upload

---
+++ operator/op5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int a = 5,b = 7;
+printf("Addiction:%d\n",a + b);
+printf("Subraction:%d\n",a - b);
+printf("Multipilcation:%d\n",a * b);
+printf("Division:%d\n",a / b);
+printf("Modulus:%d\n",a % b);
+return 0;
+}
