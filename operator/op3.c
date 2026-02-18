16f0381b78db92513ae278f36c8e066757858568
rams41168 <rams41168@gmail.com>
2026-02-18 17:29:29 +0530

    Add files via upload

---
+++ operator/op3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+int age = 20;
+float height = 5.9;
+char  grade = 'A';
+printf("age:%d\n",age);
+printf("height:%.1f\n",height);
+printf("grade:%c\n",grade);
+return 0;
+}
