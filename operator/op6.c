62931929d5dd60901122aa3a27328c418a6ba5be
rams41168 <rams41168@gmail.com>
2026-02-18 17:32:00 +0530

    Add files via upload

---
+++ operator/op6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+int a = 4,b = 7;
+printf("a==b:%d\n",a==b);
+printf("a!=b:%d\n",a!=b);
+printf("a<b:%d\n",a<b);
+printf("a>b:%d\n",a>b);
+printf("a<=b:%d\n",a<=b);
+printf("a>=b:%d\n",a>=b);
+return 0;
+}
