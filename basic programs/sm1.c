1dfb33230c442d9c16333f12382862bcf85c73e2
rams41168 <rams41168@gmail.com>
2026-02-21 22:56:36 +0530

    Add files via upload

---
+++ basic programs/sm1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+int a,b;
+int sum;
+printf("enter two value");
+scanf("%d %d",&a,&b);
+sum=a*b;
+printf("multiply value(ASCII multiplication):%d\n",sum);
+return 0;
+}
+
